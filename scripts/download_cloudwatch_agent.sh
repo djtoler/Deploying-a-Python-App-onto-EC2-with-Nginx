565cbf96540025256da5714b7490848d88df5658
Dwayne Toler (he/him) <84260551+djtoler@users.noreply.github.com>
2023-09-30 00:51:46 -0400

    Add files via upload

---
+++ scripts/download_cloudwatch_agent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://amazoncloudwatch-agent.s3.amazonaws.com/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+cd /opt/aws/amazon-cloudwatch-agent/bin/
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
